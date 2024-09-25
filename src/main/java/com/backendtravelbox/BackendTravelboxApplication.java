57ec2481c9601b2855e6cab7e0f929f060193f75
Erick130601 <erickruiztorres13062001@gmail.com>
2024-09-25 14:50:02 -0500

    change-erick

--- src/main/java/com/backendtravelbox/BackendTravelboxApplication.java
+++ src/main/java/com/backendtravelbox/BackendTravelboxApplication.java
@@ -18,7 +18,7 @@ public class BackendTravelboxApplication {
 	public WebMvcConfigurer corsconfigurer (){
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings (CorsRegistry registry) {
+			public void addCorsMappings (CorsRegistry registry) { 
 				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:4200")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
